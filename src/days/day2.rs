f75ab9c46b2468b9c4dfd08f4aca032f99775279
Henrik Andersson <2602771+snimax@users.noreply.github.com>
2024-12-02 10:00:35 +0100

    Cleaned up day 2 solution

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,103 +1,88 @@
 use advent_of_code_2024::{parse_file, parse_lines};
 
 pub fn solve() {
-  if let Ok(line_string) = parse_file("Inputs/day2.txt") {
-    let lines = parse_lines(&line_string);
-    println!("Part1 solution: {}", part1(&lines));
-    println!("Part2 solution: {}", part2(&lines));
-} else {
-    println!("Could not parse file");
-}
-}
-
-fn safe_levels(nums: &[i32]) -> bool
-{
-  let increasing = (nums[1] - nums[0]).is_positive();
-
-  for (idx, curr) in (&nums[..(nums.len()-1)]).into_iter().enumerate()
-  {
-    let next: i32 = nums[idx + 1];
-    let diff = next - curr;
-
-    let safe_gap = (1..=3).contains(&diff.abs());
-    let same_sign = if increasing { *curr < next } else { *curr > next };
-
-    if !safe_gap || !same_sign
-    {
-      return false;
+    if let Ok(line_string) = parse_file("Inputs/day2.txt") {
+        let lines = parse_lines(&line_string);
+        println!("Part1 solution: {}", part1(&lines));
+        println!("Part2 solution: {}", part2(&lines));
+    } else {
+        println!("Could not parse file");
     }
-  }
-
-  true
 }
 
-fn part1(lines: &Vec<String>) -> i32
-{
-  let mut safe_levels = 0;
-  for line in lines {
-    let mut reports = Vec::new();
-    let report_nums = line.split_ascii_whitespace();
+fn safe_levels(nums: &[i32]) -> bool {
+    let increasing = (nums[1] - nums[0]).is_positive();
 
-    for report in report_nums.into_iter() {
-      reports.push(report.parse::<i32>().unwrap());
+    for (idx, curr) in (nums[..(nums.len() - 1)]).iter().enumerate() {
+        let next: i32 = nums[idx + 1];
+        let diff = next - curr;
+
+        let safe_gap = (1..=3).contains(&diff.abs());
+        let same_sign = if increasing {
+            *curr < next
+        } else {
+            *curr > next
+        };
+
+        if !safe_gap || !same_sign {
+            return false;
+        }
     }
 
-    let positive = reports.windows(2).next()
-      .and_then(|list| Some((list[0]-list[1]).is_positive())).unwrap();
-
-    let safe_report = reports.windows(2)
-      .into_iter()
-      .all(|list| {
-        let diff = list[0] - list[1];
-        (1..=3).contains(&diff.abs())
-        && diff.is_positive() == positive
-      });
-
-    if safe_report {
-      safe_levels += 1;
-    }
-  }
-
-  safe_levels
+    true
 }
 
-fn part2(lines: &Vec<String>) -> i32
-{
-  let mut safe_level_count = 0;
-  for line in lines {
-    let mut reports = Vec::new();
-    let report_nums = line.split_ascii_whitespace();
+fn part1(lines: &Vec<String>) -> i32 {
+    let mut num_safe_reports = 0;
+    for line in lines {
+        let mut reports = Vec::new();
+        let report_nums = line.split_ascii_whitespace();
 
-    for report in report_nums.into_iter() {
-      reports.push(report.parse::<i32>().unwrap());
+        for report in report_nums.into_iter() {
+            reports.push(report.parse::<i32>().unwrap());
+        }
+
+        if safe_levels(&reports) {
+            num_safe_reports += 1;
+        }
+    }
+    num_safe_reports
+}
+
+fn part2(lines: &Vec<String>) -> i32 {
+    let mut num_safe_reports = 0;
+    for line in lines {
+        let mut reports = Vec::new();
+        let report_nums = line.split_ascii_whitespace();
+
+        for report in report_nums.into_iter() {
+            reports.push(report.parse::<i32>().unwrap());
+        }
+
+        println!("Checking {reports:?}");
+        if safe_levels(&reports) {
+            println!("{reports:?}\n");
+            num_safe_reports += 1;
+            continue;
+        }
+
+        for skip_index in 0..reports.len() {
+            let subset: Vec<i32> = reports
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i != skip_index)
+                .map(|(_, &n)| n)
+                .collect();
+
+            if safe_levels(&subset) {
+                println!("{skip_index}: {subset:?}\n");
+                num_safe_reports += 1;
+                break;
+            }
+        }
     }
 
-    println!("Checking {reports:?}");
-    if safe_levels(&reports)
-    {
-      println!("{reports:?}\n");
-      safe_level_count += 1;
-      continue;
-    }
-
-    for skip_index in 0..reports.len()
-    {
-      let subset: Vec<i32> = reports.iter()
-        .enumerate()
-        .filter(|(i, _)| *i != skip_index)
-        .map(|(_, &n)| n)
-        .collect();
-
-      if safe_levels(&subset)
-      {
-        println!("{skip_index}: {subset:?}\n");
-        safe_level_count += 1;
-        break;
-      }
-    }
-  }
-
-  safe_level_count
+    num_safe_reports
 }
 
 #[cfg(test)]
