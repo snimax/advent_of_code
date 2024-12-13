a1cc498a0ebaaa0be403923f229b811d8a68cd6b
Snimax <snimax@live.se>
2024-12-13 19:25:48 +0100

    Added equation solver function based on cramers rule

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{fs, ops};
 use std::io::Error;
-use std::{ops::Add, ops::Sub};
+use std::ops::{Add, Sub, Mul, Div};
 
 pub fn parse_file(file: &str) -> Result<String, Error> {
     fs::read_to_string(file)
@@ -84,4 +84,24 @@ impl<T: Copy> Map<T> {
         }
         true
     }
+}
+
+pub trait NumericOps: Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> + PartialEq + PartialOrd + Copy {}
+impl<T> NumericOps for T where T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + PartialEq + PartialOrd + Copy {}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Equation<T: NumericOps>
+{
+    pub x: T,
+    pub y: T,
+    pub ans: T
+}
+
+pub fn cramers_rule<T: NumericOps>(eq1: &Equation<T>, eq2: &Equation<T>) -> (T, T)
+{
+    let denominator = eq1.x*eq2.y-eq2.x*eq1.y;
+    let x_nominator = eq1.ans * eq2.y - eq1.y * eq2.ans;
+    let y_nominator = eq1.x * eq2.ans - eq1.ans * eq2.x;
+
+    (x_nominator / denominator, y_nominator / denominator)
 }
