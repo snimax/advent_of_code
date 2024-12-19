b7b7b165adc5e8525a84f773125109f6050a999d
Snimax <snimax@live.se>
2024-12-19 13:09:08 +0100

    Fixed memoized version for part 1 as well. Now they both run quickly

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,19 +1,17 @@
 use advent_of_code_2024::{parse_file, parse_lines};
 use std::collections::HashMap;
-use std::time::Instant;
 
 pub fn solve() {
     if let Ok(line_string) = parse_file("Inputs/day19.txt") {
         let lines = parse_lines(&line_string);
         let (available_patterns, patterns_to_make) = parse_towels(&lines);
-        let now = Instant::now();
-        let possible_patterns = part1(&available_patterns, &patterns_to_make);
-        println!("Part1 solution: {}", possible_patterns.len());
-        let elapsed = now.elapsed();
-        println!("{elapsed:?}");
+        println!(
+            "Part1 solution: {}",
+            part1(&available_patterns, &patterns_to_make)
+        );
         println!(
             "Part2 solution: {}",
-            part2(&available_patterns, &possible_patterns)
+            part2(&available_patterns, &patterns_to_make)
         );
     } else {
         println!("Could not parse file");
@@ -28,14 +26,6 @@ enum Color {
     Red,
     Green,
 }
-
-const COLORS: [Color; 5] = [
-    Color::White,
-    Color::Blue,
-    Color::Black,
-    Color::Red,
-    Color::Green,
-];
 
 type Towel = Vec<Color>;
 
@@ -60,67 +50,20 @@ fn parse_towels(lines: &[String]) -> (Vec<Towel>, Vec<Towel>) {
     (available_towel_patterns, patterns_to_make)
 }
 
-fn make_pattern(idx: usize, pattern: &[Color], available_patterns: &[Towel]) -> Option<()> {
-    if idx == pattern.len() {
-        return Some(());
-    }
-
-    let left_to_match = pattern.len() - idx;
-    for available_pattern in available_patterns {
-        let pattern_len = available_pattern.len();
-        if left_to_match < pattern_len {
-            continue;
-        }
-
-        if pattern[idx..(idx + pattern_len)] == *available_pattern {
-            let res = make_pattern(idx + pattern_len, pattern, available_patterns);
-            if res.is_some() {
-                return res;
-            }
-        }
-    }
-
-    None
+fn part1(available_patterns: &[Towel], patterns_to_make: &[Towel]) -> usize {
+    patterns_to_make.iter().fold(0, |acc, pattern| {
+        let mut memoization = HashMap::new();
+        acc + find_possible_patterns(0, pattern, available_patterns, &mut memoization, false)
+    })
 }
 
-fn part1(available_patterns: &[Towel], patterns_to_make: &[Towel]) -> Vec<Towel> {
-    let single_colors_available: Towel = available_patterns
-        .iter()
-        .filter(|a| a.len() == 1)
-        .map(|c| c[0])
-        .collect::<Towel>();
-
-    let look_for_patterns_with_colors: Towel = COLORS
-        .iter()
-        .filter(|c| !single_colors_available.contains(c))
-        .cloned()
-        .collect();
-
-    let patterns_to_test: Vec<Towel> = available_patterns
-        .iter()
-        .filter(|pattern| {
-            look_for_patterns_with_colors
-                .iter()
-                .any(|c| pattern.contains(c))
-                || pattern.len() == 1
-        })
-        .cloned()
-        .collect();
-
-    patterns_to_make
-        .iter()
-        .enumerate()
-        .filter_map(|(idx, pattern)| {
-            let res = make_pattern(0, pattern, &patterns_to_test);
-            if res.is_some()
-            {
-                println!("{idx}");
-                Some(pattern.clone())
-            } else {None}
-        }).collect()
-}
-
-fn count_all_possible_patterns(idx: usize, pattern: &[Color], available_patterns: &[Towel], memoization: &mut HashMap<usize, usize>) -> usize {
+fn find_possible_patterns(
+    idx: usize,
+    pattern: &[Color],
+    available_patterns: &[Towel],
+    memoization: &mut HashMap<usize, usize>,
+    find_all: bool,
+) -> usize {
     if let Some(val) = memoization.get(&idx) {
         return *val;
     }
@@ -139,7 +82,16 @@ fn count_all_possible_patterns(idx: usize, pattern: &[Color], available_patterns
         }
 
         if pattern[idx..(idx + pattern_len)] == *available_pattern {
-            result_count += count_all_possible_patterns(idx + pattern_len, pattern, available_patterns, memoization);
+            result_count += find_possible_patterns(
+                idx + pattern_len,
+                pattern,
+                available_patterns,
+                memoization,
+                find_all,
+            );
+            if !find_all && result_count == 1 {
+                return result_count;
+            }
         }
     }
 
@@ -153,15 +105,9 @@ fn count_all_possible_patterns(idx: usize, pattern: &[Color], available_patterns
 }
 
 fn part2(available_patterns: &[Towel], patterns_to_make: &[Towel]) -> usize {
-    let len = patterns_to_make.len();
-
-    patterns_to_make
-    .iter()
-    .enumerate()
-    .fold(0, |acc, (idx, pattern)| {
-        println!("{idx}/{len}");
+    patterns_to_make.iter().fold(0, |acc, pattern| {
         let mut memoization = HashMap::new();
-        acc + count_all_possible_patterns(0, pattern, &available_patterns, &mut memoization)
+        acc + find_possible_patterns(0, pattern, available_patterns, &mut memoization, true)
     })
 }
 
@@ -188,7 +134,7 @@ bbrgwb"#;
     #[test]
     fn test_part1() -> Result<(), String> {
         let (available_patterns, patterns_to_make) = get_input();
-        assert_eq!(part1(&available_patterns, &patterns_to_make).len(), 6);
+        assert_eq!(part1(&available_patterns, &patterns_to_make), 6);
 
         Ok(())
     }
