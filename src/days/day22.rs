18edcdcb5a4ac76828663b1b1124cb76c02c9f8c
Snimax <snimax@live.se>
2024-12-22 22:21:18 +0100

    Added solution for day22 part2

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -1,4 +1,5 @@
 use advent_of_code_2024::{parse_file, parse_lines};
+use std::collections::HashMap;
 
 pub fn solve() {
     if let Ok(line_string) = parse_file("Inputs/day22.txt") {
@@ -10,6 +11,8 @@ pub fn solve() {
         println!("Could not parse file");
     }
 }
+
+const ITERATIONS: usize = 2000;
 
 fn parse_initial_numbers(lines: &[String]) -> Vec<usize> {
     lines.iter().map(|s| s.parse().unwrap()).collect()
@@ -29,7 +32,7 @@ fn part1(input: &[usize]) -> usize {
     let mut result = 0;
     for &initial_secret_number in input {
         let mut curr_secret_number = initial_secret_number;
-        for _ in 0..2000 {
+        for _ in 0..ITERATIONS {
             curr_secret_number = calculate_next_secret_number(curr_secret_number);
         }
         result += curr_secret_number;
@@ -37,8 +40,67 @@ fn part1(input: &[usize]) -> usize {
     result
 }
 
-fn part2(_input: &[usize]) -> usize {
-    0
+fn generate_diff_sequence(initial_secret_number: usize) -> (Vec<usize>, Vec<i32>) {
+    let mut least_significant_digits = vec![initial_secret_number % 10; ITERATIONS + 1];
+    let mut curr_secret_number = initial_secret_number;
+    for index in 0..ITERATIONS {
+        curr_secret_number = calculate_next_secret_number(curr_secret_number);
+        least_significant_digits[index + 1] = curr_secret_number % 10;
+    }
+
+    let mut differences = vec![0; ITERATIONS];
+    for (idx, (a, b)) in least_significant_digits
+        .iter()
+        .skip(1)
+        .zip(least_significant_digits.iter())
+        .enumerate()
+    {
+        differences[idx] = *a as i32 - *b as i32;
+    }
+
+    least_significant_digits.remove(0);
+    (least_significant_digits, differences)
+}
+
+fn get_sequences(prices: &[usize], differences: &[i32]) -> HashMap<[i32; 4], usize> {
+    let mut sequence_values = HashMap::new();
+
+    for index in 3..differences.len() {
+        let sequence = [
+            differences[index - 3],
+            differences[index - 2],
+            differences[index - 1],
+            differences[index],
+        ];
+
+        if sequence_values.contains_key(&sequence) {
+            continue;
+        }
+        sequence_values.insert(sequence, prices[index]);
+    }
+
+    sequence_values
+}
+
+fn part2(input: &[usize]) -> usize {
+    let mut sequence_values = HashMap::new();
+    for &secret_num in input {
+        let (prices, differences) = generate_diff_sequence(secret_num);
+
+        let values = get_sequences(&prices, &differences);
+        for (k, v) in values {
+            if let Some(val) = sequence_values.get_mut(&k) {
+                *val += v;
+            } else {
+                sequence_values.insert(k, v);
+            }
+        }
+    }
+    if let Some((_, max_val)) = sequence_values.iter().max_by_key(|(_, v)| *v) {
+        *max_val
+    } else {
+        0
+    }
 }
 
 #[cfg(test)]
@@ -77,10 +139,20 @@ mod tests {
         assert_eq!(calculate_next_secret_number(7753432), 5908254);
     }
 
+    fn get_input2() -> Vec<usize> {
+        let input = r#"1
+2
+3
+2024"#;
+
+        let lines = parse_lines(&input);
+        parse_initial_numbers(&lines)
+    }
+
     #[test]
     fn test_part2() -> Result<(), String> {
-        let input = get_input();
-        assert_eq!(part2(&input), 0);
+        let input = get_input2();
+        assert_eq!(part2(&input), 23);
 
         Ok(())
     }
