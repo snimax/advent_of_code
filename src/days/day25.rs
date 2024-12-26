2d9804afe324646aa40f31ab0573595f2af91478
Snimax <snimax@live.se>
2024-12-26 01:33:00 +0100

    Added solution for day 25 part 1

---
+++ src/days/day25.rs
@@ -0,0 +1,149 @@
+use advent_of_code_2024::{parse_file, parse_lines};
+use std::collections::HashMap;
+
+pub fn solve() {
+    if let Ok(line_string) = parse_file("Inputs/day25.txt") {
+        let lines = parse_lines(&line_string);
+        let (keys, locks) = parse_keys_and_locks(&lines);
+        println!("Part1 solution: {}", part1(&keys, &locks));
+        println!("Part2 solution: {}", part2(&keys, &locks));
+    } else {
+        println!("Could not parse file");
+    }
+}
+
+type Key = [i32; 5];
+type Lock = [i32; 5];
+
+fn parse_keys_and_locks(lines: &[String]) -> (Vec<Key>, Vec<Lock>) {
+    let mut keys = Vec::new();
+    let mut locks = Vec::new();
+
+    let mut key_or_lock = [0; 5];
+    let mut parsing_key = false;
+    let mut check_if_parsing_key = true;
+    for line in lines {
+        if line.is_empty() {
+            // Remove the first or last line
+            key_or_lock.iter_mut().for_each(|i| {
+                *i -= 1;
+            });
+
+            if parsing_key {
+                keys.push(key_or_lock);
+            } else {
+                locks.push(key_or_lock);
+            }
+
+            key_or_lock = [0; 5];
+            check_if_parsing_key = true;
+            continue;
+        }
+        if check_if_parsing_key {
+            parsing_key = line.starts_with(".");
+            check_if_parsing_key = false;
+        }
+
+        line.chars()
+            .enumerate()
+            .filter(|(_, c)| *c == '#')
+            .for_each(|(idx, _)| {
+                key_or_lock[idx] += 1;
+            });
+    }
+
+    key_or_lock.iter_mut().for_each(|i| {
+        *i -= 1;
+    });
+
+    if parsing_key {
+        keys.push(key_or_lock);
+    } else {
+        locks.push(key_or_lock);
+    }
+
+    (keys, locks)
+}
+
+fn part1(keys: &[Key], locks: &[Lock]) -> usize {
+    let mut keys_that_fit = 0;
+    for lock in locks {
+        for key in keys {
+            if !lock.iter().zip(key).any(|(l, k)| l + k > 5) {
+                keys_that_fit += 1;
+            }
+        }
+    }
+
+    keys_that_fit
+}
+
+fn part2(_keys: &[Key], _locks: &[Lock]) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_input() -> (Vec<Key>, Vec<Lock>) {
+        let input: &str = r#"#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"#;
+
+        let lines = parse_lines(&input);
+        parse_keys_and_locks(&lines)
+    }
+
+    #[test]
+    fn test_part1() -> Result<(), String> {
+        let (keys, locks) = get_input();
+        assert_eq!(part1(&keys, &locks), 3);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), String> {
+        let (keys, locks) = get_input();
+        assert_eq!(part2(&keys, &locks), "z00,z01,z02,z05");
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ fn main() {
             Ok(22) => day22::solve(),
             Ok(23) => day23::solve(),
             Ok(24) => day24::solve(),
+            Ok(25) => day25::solve(),
             Ok(day_num) => println!("Day {day_num} not implemented yet"),
             Err(e) => panic!("Could not parse argument {e:?}"),
         },
